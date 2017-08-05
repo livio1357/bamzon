00970e21ba5113488fcfdb42a362f6137f4041a1
livio bolzon <livio@actingstudiochicago.com>
2017-08-05 18:01:07 -0500

    logging

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,44 +1,68 @@
 var mysql = require('mysql');
+var inquire = require('inquire')
 
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     passowrd: 'root',
-    database: 'bamazon'
+    database: 'bamzon'
 
 });
 
-connection.connect(function(err) {
 
-    createProduct();
+    connection.query("SELECT * FROM products",function(err, rows){
+	if (err) return err;
 
+
+	rows.forEach(function(val){
+
+		console.log("Item ID: "+val.item_id+" || Product Name: "+val.product_name+" || department name"+val.department_name+"|| Price $" +val.price +"|| Stock" +val.stock_qty);
+
+	})
+	
+	
+	
 });
 
-function createProduct() {
+//connection.connect(function(err) {
 
-    var query = connection.query(
-
-
-
-        'insert into products set ?', {
-
-            product_name:'shake wait',
-            department_name: 'fitnes',
-            price: 11.11,
-            stock_qty: 999
-
-
-        },
-
-        function(err, res) {
-            console.log(res.affectRows + 'product inserted');
-        }
+  //  createProduct();
 
 
 
-    )
+//});
+
+//function createProduct() {
+
+  //  var query = connection.query(
 
 
 
-}
+       // 'insert into products?', {
+
+         //   product_name:'shake wait',
+           // department_name: 'fitnes',
+            //price: 11.11,
+            //stock_qty: 10
+
+
+       // },
+
+       // function(err, res) {
+        //	console.log('')
+           
+      //  }
+
+
+
+  //  )
+
+
+
+//}
+
+
+
+
+
